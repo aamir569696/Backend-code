ec5b6dae4b9c1fc6f5debfd49fe770a31cf4a680
Muhammad Amir <aamir569696@gmail.com>
2026-06-11 16:43:29 +0500

    update Project

--- Day-7/src/config/Database.js
+++ Day-7/src/config/Database.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose")
 
 function connectToDb() {
-    mongoose.connect("mongodb://aamir569696_db_user:HRqNjg5ILI4Z8Mzf@ac-pcgv9ic-shard-00-00.mqoz9ii.mongodb.net:27017,ac-pcgv9ic-shard-00-01.mqoz9ii.mongodb.net:27017,ac-pcgv9ic-shard-00-02.mqoz9ii.mongodb.net:27017/?ssl=true&replicaSet=atlas-88uxw2-shard-0&authSource=admin&appName=Cluster0/")
+    mongoose.connect("day-7 connection string url")
 
     .then(()=>{
         console.log("connect To database");
